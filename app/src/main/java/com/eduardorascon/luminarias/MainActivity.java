9f0838be72e381ebd1504142b13dae743cbdb490
eduardorascon <eduardo.rascon@gmail.com>
2017-02-04 14:58:57 -0600

    Picture taken is saved.

--- app/src/main/java/com/eduardorascon/luminarias/MainActivity.java
+++ app/src/main/java/com/eduardorascon/luminarias/MainActivity.java
@@ -1,12 +1,21 @@
 package com.eduardorascon.luminarias;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +25,29 @@ public class MainActivity extends AppCompatActivity {
 
     public void launchCamera(View view) {
         Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePicture.resolveActivity(getPackageManager()) != null)
-            startActivityForResult(takePicture, 1);
+        if (takePicture.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                //e.printStackTrace();
+            }
+
+            if (photoFile != null) {
+                Uri photoUri = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
+                takePicture.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(takePicture, 1);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "Luminaria_" + timeStamp + "";
+        File storageDir = getFilesDir();
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
