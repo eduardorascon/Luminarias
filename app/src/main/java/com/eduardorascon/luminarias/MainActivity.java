b35768803769e0b14f111d30007dbd47eb354ea0
eduardorascon <eduardo.rascon@gmail.com>
2017-02-05 00:38:40 -0600

    GPS code added.

--- app/src/main/java/com/eduardorascon/luminarias/MainActivity.java
+++ app/src/main/java/com/eduardorascon/luminarias/MainActivity.java
@@ -1,30 +1,43 @@
 package com.eduardorascon.luminarias;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
+import android.widget.TextView;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity {
     String currentPhotoPath;
+    LocationManager locationManager;
+    TextView textViewLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewLocation = (TextView) findViewById(R.id.textViewLocation);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
     }
 
     public void launchCamera(View view) {
@@ -58,8 +71,73 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         return image;
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-
+    private boolean isLocationEnabled() {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
     }
+
+    private boolean checkLocation() {
+        if (isLocationEnabled() == false)
+            showAlert();
+
+        return isLocationEnabled();
+    }
+
+    private void showAlert() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Enable location")
+                .setMessage("Your location settings is set to 'Off'")
+                .setPositiveButton("Location Settings", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent locationIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(locationIntent);
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+        dialog.show();
+    }
+
+    public void toggleGPSUpdates(View view) {
+        if (checkLocation() == false)
+            return;
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2 * 60 * 1000, 10, locationListenerGPS);
+        }
+    }
+
+    private double longitudeGPS, latitudeGPS;
+    private final LocationListener locationListenerGPS = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            longitudeGPS = location.getLongitude();
+            latitudeGPS = location.getLatitude();
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    textViewLocation.setText("lat:" + latitudeGPS + ", lon:" + longitudeGPS);
+                }
+            });
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
 }
