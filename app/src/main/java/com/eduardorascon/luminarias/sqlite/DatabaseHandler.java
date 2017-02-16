1738c873dadfc8a207e47a91c13315f1c0d48fb1
eduardorascon <eduardo.rascon@gmail.com>
2017-02-16 12:27:20 -0600

    Field numero_lamparas added.

--- app/src/main/java/com/eduardorascon/luminarias/sqlite/DatabaseHandler.java
+++ app/src/main/java/com/eduardorascon/luminarias/sqlite/DatabaseHandler.java
@@ -36,6 +36,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         contentValues.put("lon", luminaria.getLon());
         contentValues.put("tipo_poste", luminaria.getTipoPoste());
         contentValues.put("tipo_lampara", luminaria.getTipoLampara());
+        contentValues.put("numero_lamparas", luminaria.getNumeroLamparas());
         return db.insert("luminarias", null, contentValues);
     }
 
@@ -80,9 +81,10 @@ public class DatabaseHandler extends SQLiteOpenHelper {
             l.setLon(cursor.getString(2));//lon
             l.setTipoPoste(cursor.getString(3));//tipo_poste
             l.setTipoLampara(cursor.getString(4));//tipo_lampara
-            l.setFechaHora(cursor.getString(5));//fecha_hora
-            l.setRespaldoImagen(cursor.getInt(6));//respaldo_imagen
-            l.setRespladoDatos(cursor.getInt(7));//resplado_datos
+            l.setNumeroLamparas(cursor.getInt(5));//numero_lamparas
+            l.setFechaHora(cursor.getString(6));//fecha_hora
+            l.setRespaldoImagen(cursor.getInt(7));//respaldo_imagen
+            l.setRespladoDatos(cursor.getInt(8));//resplado_datos
             luminariasList.add(l);
         } while (cursor.moveToNext());
 
