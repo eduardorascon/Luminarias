791e1dd3485773a6dfeb536c4e087df1a46852d4
eduardorascon <eduardo.rascon@gmail.com>
2017-02-15 20:28:21 -0600

    Splash screen implemented.

--- app/src/main/java/com/eduardorascon/luminarias/SplashScreenActivity.java
+++ app/src/main/java/com/eduardorascon/luminarias/SplashScreenActivity.java
@@ -1,13 +1,27 @@
 package com.eduardorascon.luminarias;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIME = 2800;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME);
     }
 }
